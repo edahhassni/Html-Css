36a4a8cb395e7378c208636bb968a8074f72cb78
edahhassni <edahhassni@gmail.com>
2021-04-29 11:55:11 +0100

    edah1

---
+++ main.js
@@ -0,0 +1,10 @@
+// let text = document.getElementById("text")
+// let plye = document.getElementById(".plye")
+// let remove = document.getElementById(".remove")
+// let result = document.getElementById(".result")
+
+// plye.onclick = ()=>{
+//      result = onclick("Hello");
+
+// }
+   alert("Hello Med-Moustpha")
